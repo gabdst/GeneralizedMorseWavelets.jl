44b7b4889422a56ab02fb4018f9216e7ce6c2a77
gabriel destouet <gabriel.destouet@protonmail.com>
2024-02-12 15:27:15 +0100

    update doc

--- src/gmw.jl
+++ src/gmw.jl
@@ -13,8 +13,6 @@ function wdomain(a::Real,N::Integer,M::Integer=N)
   return domain
 end
 
-
-
 """
   wdomain!(domain,a::Real,N::Integer,M::Integer=N)
 
@@ -115,7 +113,7 @@ duration(g_params) = duration(g_params[3],g_params[4])
 """
   gmw_grid(β, γ, J, Q; wmin=0,wmax=pi,phase=0.)
 
-Creates a grid of generalized morse wavelets. The grid starts by initiliazing the first wavelet at the lowest scalce with frequency peak `wmax`, it then generates subsequent wavelets by scaling it by a factor `2^(1/JQ)`. `J`is the number of octaves and `Q` the number of inter-octaves. It results in a grid of `JQ` wavelets.
+Creates a grid of generalized morse wavelets. The grid starts by initiliazing the first wavelet at the lowest scale with frequency peak `wmax`, it then generates subsequent wavelets by scaling it by a factor `2^(1/JQ)`. `J` is the number of octaves and `Q` the number of inter-octaves. It results in a grid of `JQ` wavelets.
 """
 function gmw_grid(β,γ,J,Q,wmin=0,wmax = pi,phase=0.)
   logpsi = log(w_psi(β,γ))
